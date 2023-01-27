19f9b7398017414f492bbd4bf7832f32c5c3f21d
mikzanco <mikyzancolo97@gmail.com>
2023-01-27 09:33:38 +0100

    milestone 1 fatta, milestone 2 fatta: dati visibili correttamente in Thunder Client

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::all();
-        return response()->json(compact('Projects'));
+        $projects = Projects::all();
+        return response()->json(compact('projects'));
     }
 }
